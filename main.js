25298e644f1687b53cce9adf672ffc8e8ddfc912
Brahm Prakash Mishra <bpmishra92@gmail.com>
2022-04-19 23:46:12 +0530

    express routing

--- main.js
+++ main.js
@@ -1,29 +1,11 @@
-const http = require('http')
-const port = process.env.PORT || 3000
+const express = require('express')
+const path = require('path')
 
-const fs = require('fs')
+const app = express()
+app.use(express.static('public'))
+app.listen((process.env.PORT || 3000), () => console.log("App listening on port 3000"))
 
-const homePage = fs.readFileSync('index.html')
-const aboutPage = fs.readFileSync('about.html')
-const notFoundPage = fs.readFileSync('notFound.html')
-const contactPage = fs.readFileSync('contact.html')
-
-function server_callback(req, res) {
-    switch(req.url) {
-        case '/about':
-            res.end(aboutPage)
-            break
-        case '/contact':
-            res.end(contactPage)
-            break
-        case '/' :
-            res.end(homePage)
-            break
-        default:
-            res.writeHead(404)
-            res.end(notFoundPage)
-    }
-}
-
-const server = http.createServer(server_callback)
-server.listen(port)
+app.get("/", (req, res) => {
+    console.log(req)
+    res.sendFile(path.resolve(__dirname,'index.html'))
+})
